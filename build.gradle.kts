946c2aac68eabff71867d6baacb79b63261fae48
Igor Alyoshin <stawr88@bk.ru>
2021-09-22 16:20:57 +0300

    kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,5 @@ allprojects {
   repositories {
     google()
     mavenCentral()
-    maven { url = uri("https://jitpack.io") }
   }
-}
-
-tasks.register("clean", Delete::class) {
-  delete(rootProject.buildDir)
 }
